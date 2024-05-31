d5017b805b3e75fd05babffa4d1f1317da3630ba
WaShindeiru <franekdata@gmail.com>
2024-05-31 09:37:04 +0200

    remove redundant code

--- src/main/java/client/RequestDeserializer.java
+++ src/main/java/client/RequestDeserializer.java
@@ -13,15 +13,6 @@ public class RequestDeserializer implements JsonDeserializer<Request> {
         JsonElement requestType = jsonObject.get("type");
         JsonElement key = jsonObject.get("key");
         JsonElement value = jsonObject.get("value");
-        Gson gson = new Gson();
-
-        // value is a single value, transform it into an array
-        if (value != null && value.isJsonPrimitive()) {
-            String valueString = value.getAsString();
-            value = gson.fromJson(valueString, JsonElement.class);
-        } else if (value != null && value.isJsonObject()) {
-            value = value.getAsJsonObject();
-        }
 
         return new Request(requestType.getAsString(), key, value);
     }
